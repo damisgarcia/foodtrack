460dfbc1143cbf2d83d6d851f354dae8f906bbdb
damisgarcia <damisgarcia@gmail.com>
2015-06-15 12:19:07 -0300

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foodtrackwebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the foodtrackwebApp
+ */
+angular.module('foodtrackwebApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foodtrackwebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the foodtrackwebApp
+ */
+angular.module('foodtrackwebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
