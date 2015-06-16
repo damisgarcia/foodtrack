88baddfcddf848c5ff2c2e2e5e2daef554324b30
damisgarcia <damisgarcia@gmail.com>
2015-06-16 14:42:05 -0300

    Add Last Posts Facebook

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,55 @@
  * Controller of the foodtrackwebApp
  */
 angular.module('foodtrackwebApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,Facebook) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    // Fan Pages
+    // BrasilFoodTrucks:"244576275632539"
+    // GuiaFoodTrucks:"1491757611043874"
+    // FoodTruckNasRuas:"232223216969714"
+
+    $scope.fanpages = [
+      {id:"244576275632539",posts:[]},
+      {id:"1491757611043874",posts:[]},
+      {id:"232223216969714",posts:[]}
+    ]
+
+    $scope.posts = {
+      grids:[
+        { objects:[] },{objects:[]},{ objects:[] }
+      ]
+    }
+    var count = 0;
+    // Requisitando últimas postagens
+    angular.forEach($scope.fanpages,function(fanpage,index,_array){
+      Facebook.getPostsFanPage(fanpage.id,15,function(result, status, headers, config) {
+        angular.forEach(result.data,function(post,index,_array){
+          Facebook.getImageFromPost(post.object_id,function(object, status, headers, config){
+            // Populando e Randomizando a View
+            if(object.images != undefined)
+              $scope.posts.grids[count].objects.push(object)
+              count == 2 ? count = 0 : count++
+              $scope.$apply() //update view
+              if(index == (_array.length - 1)){
+                angular.forEach($scope.posts.grids,function(grid,index){
+                  $scope.posts.grids[index] = shuffle(grid)
+                })
+              }
+
+          });
+        });
+      });
+    });
+
+    // Embaralhando Array
+    function shuffle(o){
+      for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+      return o;
+    }
+
   });
---
+++ app/scripts/services/facebook.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name foodtrackwebApp.Facebook
+ * @description
+ * # Facebook
+ * Factory in the foodtrackwebApp.
+ */
+angular.module('foodtrackwebApp')
+  .factory('Facebook', function ($http) {
+
+    var Facebook = (function(){
+      var self = {}
+      // Service logic
+      var domain = "https://graph.facebook.com/"
+      var version = "v2.3/"
+      var accessToken = "494196217405022|o40ok_IbWRmbn40eLOowbeVaqSw"
+
+      self.getPostsFanPage = function (FanPageId,limit,callback) {
+        var path = "/posts?"
+        if(FanPageId != undefined || FanPageId != null){
+          $http.get( domain + version + FanPageId + path + limit +"&"+ "access_token=" + accessToken )
+          .success(callback)
+          .error(HttpException)
+        }
+      }
+
+      self.getImageFromPost = function (objectID,callback) {
+        if(objectID != undefined || objectID != null){
+          $http.get( domain + version + objectID + "?" + "access_token=" + accessToken )
+          .success(callback)
+          .error(HttpException)
+        }
+      }
+      // private
+      function HttpException(data, status, headers, config){
+        return { error:data, status: status }
+      }
+      return self;
+    }(Facebook));
+
+    // Public API here
+    return Facebook;
+  });
